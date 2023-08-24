91253d67c81bab560dc24c45fb6329169bb9c21e
Shaik Mohammed Junaid <132887226+junaidshaik2600@users.noreply.github.com>
2023-08-24 18:54:54 +0530

    update portfolio
    i have change the ui design of my portfolio  and know it looks like professional portfolio.
    still have to add move like : 1. about page 2. home page 3. project page

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-// function onClickMenu() {
-//   document.getElementById("menu").classList.toggle("icon");
-//   document.getElementById("nav").classList.toggle("icon");
-// }
-
+// for nav menu
 function onClickMenu() {
-  const menuIcon = document.getElementById("menu");
-  const navList = document.getElementById("nav");
+    const menuIcon = document.getElementById("menu");
+    const navList = document.getElementById("nav");
+  
+    menuIcon.classList.toggle("icon");
+    navList.classList.toggle("icon");
+  }
+// typed script
+  var typed = new Typed("#element", {
+    strings: ["Java Developer", " And ", "Web Developer"],
+    typeSpeed: 80,
+  });
 
+// Get references to the menu icon and navigation element
+const menuIcon = document.getElementById("menu");
+const navList = document.getElementById("nav");
+
+// Function to close the navigation menu
+function closeNavMenu() {
+  menuIcon.classList.remove("icon");
+  navList.classList.remove("icon");
+}
+
+// Event listener for clicks on the document
+document.addEventListener("click", function (event) {
+  // Check if the clicked element is outside the navigation area
+  if (!menuIcon.contains(event.target) && !navList.contains(event.target)) {
+    closeNavMenu(); // Close the navigation menu
+  }
+});
+
+// Event listener for scrolls on the document
+document.addEventListener("scroll", function (event) {
+  // Check if the scroll occurred outside the navigation area
+  if (!menuIcon.contains(event.target) && !navList.contains(event.target)) {
+    closeNavMenu(); // Close the navigation menu
+  }
+});
+
+// Function to toggle the navigation menu when the menu icon is clicked
+function onClickMenu() {
   menuIcon.classList.toggle("icon");
   navList.classList.toggle("icon");
 }

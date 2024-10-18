31cf91d4abda692370298f891100fe23a97f77fb
junaidshaik2600 <junaidshaik@gmail.com>
2024-10-18 23:36:44 +0530

    Revise portfolio website with new JavaScript skills and enhanced structure
    Updated my portfolio website to reflect new JavaScript skills, incorporating a more organized folder structure for improved maintainability. Implemented a global navigation system that appears across all pages, enhancing user experience and site navigation. Additionally, updated styles to create a cohesive and visually appealing design throughout the site. This commit represents a significant step in showcasing my development skills and ensuring a more user-friendly interface.

---
+++ Assets/js/nav.js
@@ -0,0 +1,59 @@
+const nav = document.querySelector(".right");
+const navHtml = `
+    <div id="menu" class="menu-button" onclick="onClickMenu()">
+    <div id="bar1" class="bar"></div>
+    <div id="bar2" class="bar"></div>
+    <div id="bar3" class="bar"></div>
+    </div>
+    <ul class="nav" id="nav">
+    <li><a href="/index.html">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="/pages/projects.html">Projects</a></li>
+    <li><a href="/pages/team.html">Team</a></li>
+    <li><a href="/pages/contact.html">Contact Me</a></li>
+    </ul>`;
+if (nav) {
+  nav.innerHTML = navHtml;
+}
+
+// for nav menu
+function onClickMenu() {
+    const menuIcon = document.getElementById("menu");
+    const navList = document.getElementById("nav");
+    
+    menuIcon.classList.toggle("icon");
+    navList.classList.toggle("icon");
+  }
+  
+  // Get references to the menu icon and navigation element
+  const menuIcon = document.getElementById("menu");
+  const navList = document.getElementById("nav");
+  
+  // Function to close the navigation menu
+  function closeNavMenu() {
+    menuIcon.classList.remove("icon");
+    navList.classList.remove("icon");
+  }
+  
+  // Event listener for clicks on the document
+  document.addEventListener("click", function (event) {
+    // Check if the clicked element is outside the navigation area
+    if (!menuIcon.contains(event.target) && !navList.contains(event.target)) {
+      closeNavMenu(); // Close the navigation menu
+    }
+  });
+  
+  // Event listener for scrolls on the document
+  document.addEventListener("scroll", function (event) {
+    // Check if the scroll occurred outside the navigation area
+    if (!menuIcon.contains(event.target) && !navList.contains(event.target)) {
+      closeNavMenu(); // Close the navigation menu
+    }
+  });
+  
+  // Function to toggle the navigation menu when the menu icon is clicked
+  function onClickMenu() {
+    menuIcon.classList.toggle("icon");
+    navList.classList.toggle("icon");
+  }
+  

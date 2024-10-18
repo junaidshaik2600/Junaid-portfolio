ab400a10f088b6e8aef0ea6c5c722c54231a8b2d
junaidshaik2600 <junaidshaik@gmail.com>
2024-10-19 00:03:17 +0530

    fix - some bug
    page navigation problem

---
+++ Assets/js/projects.js
@@ -0,0 +1,30 @@
+const allProjects = document.querySelector(".allProjects");
+
+// console.log(grid);
+let response;
+const file = "/data/projectsData.json";
+fetch(file)
+  .then((res) => {
+    return res.json();
+  })
+  .then((res) => {
+    response = res;
+    handler(response);
+  });
+let htmlContent = "";
+function handler(value) {
+  let data = value;
+
+  for (const dataValue of data) {
+    let thisValue = `
+        <div class="project">
+            <img src="${dataValue.imgSrc}" alt="" class="imgLogo" />
+            <h3 class="title">${dataValue.titleText}</h3>
+            <strong><a href="${dataValue.link}" class="link">View-Project</a></strong>
+        </div>
+        `;
+    htmlContent += thisValue;
+
+    allProjects.innerHTML = htmlContent;
+  }
+}
--- README.txt
+++
@@ -1,3 +0,0 @@
-Hello This Is An Portfolio Of Me ( Shaik Mohammad Junaid )
-And I am Learing Web Development new Skill Every Day And Improving My Self Day By Day
-I Introduce My-Self Life-Time Learner

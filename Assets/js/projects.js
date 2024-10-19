762348614fdae86c431339e791081a4165dee397
Shaik Mohammed Junaid <132887226+junaidshaik2600@users.noreply.github.com>
2024-10-19 12:32:40 +0530

    Update projects.js

--- Assets/js/projects.js
+++ Assets/js/projects.js
@@ -2,7 +2,7 @@ const allProjects = document.querySelector(".allProjects");
 
 // console.log(grid);
 let response;
-const file = "./data//projectsData.json";
+const file = "./data/projectsData.json";
 fetch(file)
   .then((res) => {
     conssole.log(res);
